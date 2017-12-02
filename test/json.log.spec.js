a790d3292e5985c75ee5a47fb9a334d72bd07abc
Onur Yıldırım <onur@cutepilot.com>
2017-12-03 02:36:48 +0300

    minor

--- test/json.log.spec.js
+++ test/json.log.spec.js
@@ -3,10 +3,10 @@
 
     const json = require('../index');
 
-    xdescribe('json-more logs', function () {
+    describe('json-more logs', function () {
 
         it('should log', function () {
-            json.log('This test does not assert. Check logs...');
+            json.log('This test does not assert. just these logs...');
             json.log.pretty({ log: 'log', pretty: true }, 'extra arg', null);
             json.info.pretty({ log: 'info', pretty: true });
             json.warn.pretty({ log: 'warn', pretty: true });
